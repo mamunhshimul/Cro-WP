7d8bd7835ac7d3d728f691b8acd468edcf4a7ed8
ahnuman <ahnuman5@gmail.comgigit config --global user.email ahnuman5@gmail.com>
2019-02-18 03:06:41 +0600

    added basic wordpress functions

--- functions.php
+++ functions.php
@@ -1,5 +1,14 @@
 <?php
 function cro_after_setup(){
     load_theme_textdomain( "cro" );
+    add_theme_support( "title-tag" );
+    add_theme_support( "post_thumbnails" );
 }
 add_action( "after_setup_theme", "cro_after_setup" );
+
+
+function cro_all_css_and_js(){
+    wp_enqueue_style( "cro", get_stylesheet_url() );
+    
+}
+add_action( "wp_enqueue_scripts", "cro_all_css_and_js" );
